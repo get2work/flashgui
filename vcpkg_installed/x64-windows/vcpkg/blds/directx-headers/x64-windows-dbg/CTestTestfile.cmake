b30cdc3bf9bb7d66bd312ea73fdf0439f85c5d97
get2work <47090811+get2work@users.noreply.github.com>
2025-10-01 11:31:20 -0400

    Delete vcpkg_installed directory

--- vcpkg_installed/x64-windows/vcpkg/blds/directx-headers/x64-windows-dbg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/dev.DESKTOP-Q7BA7UO/source/repos/get2work/flashgui/vcpkg_installed/x64-windows/vcpkg/blds/directx-headers/src/v1.615.0-beb88d0f38.clean
-# Build directory: C:/Users/dev.DESKTOP-Q7BA7UO/source/repos/get2work/flashgui/vcpkg_installed/x64-windows/vcpkg/blds/directx-headers/x64-windows-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
